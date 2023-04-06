f3ac3c92e2557a334b7344eda7485cc99aad0d40
harshgoswam <122149827+harshgoswam@users.noreply.github.com>
2023-04-06 11:06:46 +0530

    Add files via upload

---
+++ String1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+	char ch='H',ch1='A',ch2='R',ch3='S',ch4='H';
+	
+	printf("%c",ch+32);
+	printf("\n%c",ch1+32);
+	printf("\n%c",ch2+32);
+	printf("\n%c",ch3+32);
+	printf("\n%c",ch4+32);	
+}
---
+++ String2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+	char ch='h',ch1='a',ch2='r',ch3='s',ch4='h';
+	
+	printf("%c",ch-32);
+	printf("\n%c",ch1-32);
+	printf("\n%c",ch2-32);
+	printf("\n%c",ch3-32);
+	printf("\n%c",ch4-32);	
+}
